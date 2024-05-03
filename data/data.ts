474d51427946ffdf3eaebd57afdda1eee661c17d
Jersy Fernandes <102835855+JersyFernandesJF@users.noreply.github.com>
2024-05-03 14:30:23 +0100

    first commit

---
+++ data/data.ts
@@ -0,0 +1,21 @@
+import heros from "./heros";
+import stories from "./stories";
+import promos from "./promos";
+import features from "./features";
+import pricing from "./pricing";
+import compare from "./compare";
+import banners from "./banners";
+import routes from "./routes";
+
+const data = {
+  routes: routes,
+  heros: heros,
+  promos: promos,
+  stories: stories,
+  features: features,
+  pricing: pricing,
+  compare: compare,
+  banners: banners,
+};
+
+export default data;
