f96259d57327f7254ff9821148533d896aff4a0e
Jersy Fernandes <102835855+JersyFernandesJF@users.noreply.github.com>
2024-05-03 15:48:32 +0100

    button primary

---
+++ src/components/buttons/primary-button.tsx
@@ -0,0 +1,26 @@
+import Link, { LinkProps } from "next/link";
+import React from "react";
+
+type Props = LinkProps & {
+  children: React.ReactNode;
+  bgColor?: "white" | "black";
+};
+
+const PrimaryButton = React.forwardRef<HTMLAnchorElement, Props>(
+  ({ children, bgColor = "black", ...props }, ref) => {
+    const blackTheme =
+      "bg-brand-black text-brand-white hover:bg-brand-grey hover:text-brand-black";
+    const whiteTheme =
+      "bg-brand-white text-brand-black hover:bg-brand-grey hover:text-brand-black";
+    const theme = bgColor === "black" ? blackTheme : whiteTheme;
+    return (
+      <Link
+        {...props}
+        ref={ref}
+        className={`flex h-10 w-full min-w-[158px] transform items-center justify-center ${theme} text-center text-xs font-bold uppercase tracking-[2px] transition-colors`}
+      >
+        {children}
+      </Link>
+    );
+  }
+);
